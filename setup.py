487a76f84e97b9919cf493ef972a7ede9dd66636
pranavsrinivasvenkatesh <pranav10srinivas@gmail.com>
2025-11-23 17:54:43 +0100

    Updated Folder structure

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "Kidney-Disease-Classification-Deep-Learning-Project"
-AUTHOR_USER_NAME = "krishnaik06"
+REPO_NAME = "Kindey-Disease-Classification-MLOPS-Project"
+AUTHOR_USER_NAME = "pranavsrinivas29"
 SRC_REPO = "cnnClassifier"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
+AUTHOR_EMAIL = "pranav10srinivas@gmail.com"
 
 
 setuptools.setup(
--- template.py
+++ template.py
@@ -4,7 +4,7 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]:%(message)s:')
 
-project_name = 'cnnClasssifier'
+project_name = 'cnnClassifier'
 
 list_of_files = [
     ".github/workflows/.gitkeep",
